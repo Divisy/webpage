c9ed6781d0473a3084e425bf14ab450988c63645
Divisy <rafael.tuta.salinas@gmail.com>
2023-05-01 17:21:06 +0200

    add gtm

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from "vue";
+import { createGtm } from '@gtm-support/vue-gtm';
 import App from "./App.vue";
 import router from "./router";
 import AOS from "aos";
@@ -24,14 +25,6 @@ app.use(
         gtm_auth: '',
         gtm_preview: 'env-4',
         gtm_cookies_win: 'x',
-      },
-      source: 'https://divisy.co/gtm.js', // Add your own serverside GTM script
-      defer: false, // Script can be set to `defer` to speed up page load at the cost of less accurate results (in case visitor leaves before script is loaded, which is unlikely but possible). Defaults to false, so the script is loaded `async` by default
-      compatibility: false, // Will add `async` and `defer` to the script tag to not block requests for old browsers that do not support `async`
-      nonce: '7777', // Will add `nonce` to the script tag
-      enabled: true, // defaults to true. Plugin can be disabled by setting this to false for Ex: enabled: !!GDPR_Cookie (optional)
-      debug: true, // Whether or not display console logs debugs (optional)
-      loadScript: true, // Whether or not to load the GTM Script (Helpful if you are including GTM manually, but need the dataLayer functionality in your components) (optional)
-      trackOnNextTick: false, // Whether or not call trackView in Vue.nextTick
-    }),
+      }
+    })
   );
